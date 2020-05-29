6a7f4556201493d2d39ae364e08c243909f2f055
bpasn <57083511+bpasn@users.noreply.github.com>
2020-05-29 22:55:36 +0700

    Create test-login.page.ts

---
+++ src/app/test-login/test-login.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-login',
+  templateUrl: './test-login.page.html',
+  styleUrls: ['./test-login.page.scss'],
+})
+export class TestLoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
