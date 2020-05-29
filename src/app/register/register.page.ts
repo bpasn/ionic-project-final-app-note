7b038ba95965575f50662e2263af41f48c2d8e55
bpasn <57083511+bpasn@users.noreply.github.com>
2020-05-30 02:31:05 +0700

    Update register.page.ts

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -4,7 +4,9 @@ import { auth } from "firebase/app";
 import {AngularFirestore} from "@angular/fire/firestore";
 import { AlertController } from "@ionic/angular";
 import { Router } from "@angular/router";
+import { UserService } from '../user.service';
 // import { UserService } from 'src/app/user.service';
+
 @Component({
   selector: "app-register",
   templateUrl: "./register.page.html",
@@ -18,44 +20,65 @@ export class RegisterPage implements OnInit {
   constructor(
     public afAuth: AngularFireAuth,
     public afstore: AngularFirestore,
-   
-    public alert: AlertController,
+    public alertController: AlertController,
     public router: Router,
-     // public user: UserService,
+     public user: UserService,
   ) {}
 
   ngOnInit() {}
 
+  async presentAlert(title: string, content: string) {
+    const alert = await this.alertController.create({
+      header: title,
+      message: content,
+      buttons: ['OK'],
+    });
+
+    await alert.present()
+  }
+
   async register() {
     const { username, password, cpassword } = this
     if (password !== cpassword) {
-      this.showtAlert("error!","password don't match")
+      //ยังไใ่ใช้ Alert ตัวนี้
+      // this.showtAlert("error!","password don't match")
       return console.error("Password don't match");
     }
 
     try {
-      const res = await this.afAuth.auth.createUserWithEmailAndPassword(username+"@codemamn.com",password);
+      const res = await this.afAuth.auth.createUserWithEmailAndPassword(username +"@codedamn.com",password);
       console.log(res)
-      this.showtAlert("Success!", "Welcome board")
+
+      this.afstore.doc(`users/${res.user.uid}`).set({
+        username
+
+      })
+
+      this.user.setUser({
+        username,
+        uid: res.user.uid
+
+      })
+
+      this.presentAlert('Success', 'You are registered!')
       this.router.navigate(['/tabs'])
+
+      // ยังไม่ได้ใช้ Alert
+      // this.showtAlert("Success!", "Welcome board")
+      // this.router.navigate(['/tabs'])
 
     } catch (error) {
       console.dir(error)
-      this.showtAlert("Error", error.message)
+
+
+       // ยังไม่ได้ใช้ Alert
+      // this.showtAlert("Error", error.message)
     }
 
   }
 
+// ยังไม่ได้ใช้ Alert ตัวนี้
 
-  async showtAlert(header: string, message: string) {
-    const alert = await this.alert.create({
-      header,
-      message,
-      buttons: ["OK"],
-    });
-
-    await alert.present();
-  }
 
 }
 
