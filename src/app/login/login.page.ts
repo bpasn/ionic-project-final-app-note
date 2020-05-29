012dc87dede9e94add8a00ae047d9b89076d546f
bpasn <57083511+bpasn@users.noreply.github.com>
2020-05-30 02:31:12 +0700

    Update login.page.ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth'
  import { auth } from 'firebase/app'
  import { AlertController } from "@ionic/angular";
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: "app-login",
@@ -15,7 +17,10 @@ export class LoginPage implements OnInit {
 
   constructor(
     public afAuth: AngularFireAuth,
-    public alertController: AlertController
+    public alertController: AlertController,
+    public user: UserService,
+    public router: Router,
+
   ) {}
 
   ngOnInit() {}
@@ -25,17 +30,30 @@ export class LoginPage implements OnInit {
     
     try {
       // kind of a hack.
-      const res = await this.afAuth.auth.signInWithEmailAndPassword(
-        username + "@codedamn.com",
-        password
-      );
+      const res = await this.afAuth.auth.signInWithEmailAndPassword(username + "@codedamn.com",password)
+
+
+      if(res.user) {
+        this.user.setUser({
+          username,
+          uid: res.user.uid
+        })
+        this.router.navigate(['/tabs'])
+        
+      }
+
+
     } catch (err) {
       console.dir(err);
       if (err.code === "auth/operation-not-allowed") {
         console.log("yser don't found");
+
       }
+
     }
+
   }
+
 
 
 }
