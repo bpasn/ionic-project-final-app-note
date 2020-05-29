af51c81f97980dc66db32b5edb835abb22b47b3e
bpasn <57083511+bpasn@users.noreply.github.com>
2020-05-29 22:54:25 +0700

    Create firebase.ts

---
+++ src/app/firebase.ts
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyA4szSmHalAmep8ae2Uw18N5Uey2TwvGPE",
+  authDomain: "fir-9b649.firebaseapp.com",
+  databaseURL: "https://fir-9b649.firebaseio.com",
+  projectId: "fir-9b649",
+  storageBucket: "fir-9b649.appspot.com",
+  messagingSenderId: "836209443391",
+  appId: "1:836209443391:web:d9c3b53ebffaddb7d92e8a",
+  measurementId: "G-B2WWZVE65X",
+};
+export default config
