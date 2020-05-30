0ca91d6ba09783edbb08ddc4ddba86fd4e9dcf54
bpasn <57083511+bpasn@users.noreply.github.com>
2020-05-30 20:39:26 +0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { AuthService } from './auth.service'
 const routes: Routes = [
-  { path: "", redirectTo: "tabs", pathMatch: "full" },
+  { path: "", redirectTo: "login", pathMatch: "full" },
   { path: "login", loadChildren: "./login/login.module#LoginPageModule" },
   {path: "register",loadChildren: "./register/register.module#RegisterPageModule"},
-  {path: "tabs",loadChildren: "./tabs/tabs.module#TabsPageModule"},
-    // canActivate: [AuthService],
+  { path: "tabs", loadChildren: "./tabs/tabs.module#TabsPageModule", canActivate: [AuthService] },
+  {
+    path: 'add-note',
+    loadChildren: () => import('./add-note/add-note.module').then( m => m.AddNotePageModule)
+  },
+  {
+    path: 'view-note/:id',
+    loadChildren: () => import('./view-note/view-note.module').then( m => m.ViewNotePageModule)
+  },
+  {
+    path: 'update-note/:id',
+    loadChildren: () => import('./update-note/update-note.module').then( m => m.UpdateNotePageModule)
+  },
+  
+
  
 
 ];
@@ -17,3 +30,7 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 // , { preloadingStrategy: PreloadAllModules 
+// {
+//   path: 'post',
+//     loadChildren: () => import('./post/post.module').then(m => m.PostPageModule)
+// },
