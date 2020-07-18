cba92ec078b3b0082be5a7ae1373ea60393b1f68
bpasn <57083511+bpasn@users.noreply.github.com>
2020-07-18 19:21:08 +0700

    Update profile.module.ts

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { ProfilePageRoutingModule } from './profile-routing.module';
 
 import { ProfilePage } from './profile.page';
+import { ShareModule } from '../share.module';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { ProfilePage } from './profile.page';
     FormsModule,
     IonicModule,
     ProfilePageRoutingModule,
+    ShareModule,
   
   ],
   declarations: [ProfilePage]
