4efd3f5f7ca6d1a8a84708861a9ff1b31694677e
bpasn <57083511+bpasn@users.noreply.github.com>
2020-08-24 20:04:58 +0700

    Update update-note.page.ts

--- src/app/update-note/update-note.page.ts
+++ src/app/update-note/update-note.page.ts
@@ -1,20 +1,4 @@
 
-//CODE ต้นฉบับ
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-update-note',
-//   templateUrl: './update-note.page.html',
-//   styleUrls: ['./update-note.page.scss'],
-// })
-// export class UpdateNotePage implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit() {
-//   }
-
-// }
 
 
 import { AfterViewInit, Component, OnInit } from '@angular/core';
